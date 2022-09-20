e7cd11b9815270c0854f57aaf559b32c57e39e32
Your Name <dasdsa>
2022-09-20 16:44:37 +0700

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::VecDeque;
+use std::sync::{Arc, Condvar, Mutex};
+
+pub struct Inner<T> {
+    queue: Mutex<VecDeque<T>>,
+}
+
+pub struct Sender<T> {
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(&mut self, message: T) {
+        let queue = self.inner.queue.lock().unwrap();
+        queue.push_back(message);
+    }
+}
+
+pub struct Receiver<T> {
+    inner: Arc<Inner<T>>,
+}
+
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        let queue = self.inner.queue.lock().unwrap();
+        message = queue.pop_front();
+    }
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Inner {
+        queue: Mutex::default(),
+    };
+    let inner = Arc::new(inner);
+    (
+        Sender {
+            inner: inner.clone(),
+        },
+        Receiver {
+            inner: inner.clone(),
+        },
+    )
 }
 
 #[cfg(test)]
@@ -8,7 +48,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let (sender, receiver) = channel::<i32>();
     }
 }
